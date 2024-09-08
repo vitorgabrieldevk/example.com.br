d3eb286c25623544cab1c4a27693857c2925de6d
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2024-09-08 11:12:27 -0300

    feat: :sparkles: teste

--- site/src/app/sobre/page.tsx
+++ site/src/app/sobre/page.tsx
@@ -2,13 +2,8 @@
 import RootLayout from "../layout";
 
 export default function Sobre() {
-  const metadata = {
-    title: "Sobre Nós", 
-    description: "Saiba mais sobre nossa empresa e equipe",
-  };
-
   return (
-    <RootLayout metadata={metadata}>
+    <RootLayout>
       <div className="container">
         <h1>Sobre Nós</h1>
         <p>Bem-vindo à página sobre nós!</p>
