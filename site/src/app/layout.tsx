c46b5241a7809bf28941d0eb9daae144085119f3
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2024-09-08 11:11:17 -0300

    feat: :sparkles: METADATA

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,17 +1,17 @@
 // app/layout.tsx
-import type { Metadata } from "next";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-  metadata?: Metadata;
 }) {
   return (
     <html lang="pt-br">
       <head>
+        <title>Seu Título Padrão</title>
+        <meta name="description" content="Descrição padrão da página" />
       </head>
       <body>
         {children}
